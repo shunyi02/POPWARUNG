86f95933f3460d538d0582c2b22b029d809e9212
ShunYi <nshunyi02@gmail.com>
2025-05-18 07:29:17 +0800

    change the tab name

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -41,13 +41,13 @@ export default function Home() {
   return (
     <>
       <PageMeta
-        title="React.js Ecommerce Dashboard | TailAdmin - React.js Admin Dashboard Template"
-        description="This is React.js Ecommerce Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+        title="POPWARUNG"
+        description="A smart AI-powered inventory and sales forecasting system for retail stores."
       />
       {/* Add QuickBI View */}
       <div className="col-span-12 rounded-2xl border border-gray-200 bg-white dark:border-gray-800 dark:bg-white/[0.03]">
         <iframe
-          src="https://bi-cn-hongkong.data.aliyun.com/token3rd/dashboard/view/pc.htm?pageId=a211e2cb-6ebf-493f-883d-a96f9ee2134e&accessTicket=f7b59faa-429f-46a2-92bb-6bedf650f263&dd_orientation=auto "
+          src="https://bi-cn-hongkong.data.aliyun.com/token3rd/dashboard/view/pc.htm?pageId=4001c9cd-7066-4d1a-9da9-16a219c3778f&accessTicket=c1784bce-b9cb-4c43-ae76-504e767003c7&dd_orientation=auto "
           className="w-full h-[600px] rounded-2xl"
           frameBorder="0"
           allowFullScreen
